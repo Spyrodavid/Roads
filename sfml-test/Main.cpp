8910787c7d72d172e7a8a6b563b136f7ecf4bccc
David Saifer <spyrotheultimate@gmail.com>
2023-02-20 20:59:32 -0800

    working dots

---
+++ sfml-test/Main.cpp
@@ -0,0 +1,87 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <vector>
+#include "Solver.hpp"
+#include "Renderer.hpp"
+
+using namespace std;
+
+int main()
+{
+    // Setup window
+    int window_width = 800;
+    int window_height = 800;
+
+
+    sf::RenderWindow window(sf::VideoMode(window_width, window_height), "Verlet Integration!!");
+
+
+    // setup vars for whole program
+    Solver solver;
+    Renderer renderer{ window };
+    sf::Vector2f middle_constraint = { static_cast<float> (window_width / 2), static_cast<float> (window_height / 2) };
+    float object_spawn_rate = .01;
+    int object_max = 300;
+
+    // add constraint to solver
+    solver.setConstraint(middle_constraint, static_cast<float> (window_height / 2));
+
+
+
+    vector<sf::Time> frames_in_second_vector;
+
+    sf::Clock clock;
+
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            switch (event.type) {
+                case sf::Event::Closed:
+                    window.close();
+                    break;
+                case sf::Event::KeyPressed:
+                    if (event.key.code == sf::Keyboard::Equal) {
+                        auto constraint = solver.getConstraint();
+                        solver.setConstraint({ constraint.x, constraint.y }, constraint.z - 1);
+                    }
+                    if (event.key.code == sf::Keyboard::Hyphen) {
+                        auto constraint = solver.getConstraint();
+                        solver.setConstraint({ constraint.x, constraint.y }, constraint.z + 1);
+                    }
+
+                    break;
+                default:
+                    break;
+            }
+            
+        }
+
+        while (clock.getElapsedTime().asSeconds() > solver.getObjectCount() * object_spawn_rate && solver.getObjectCount() < object_max) {
+            auto& object = solver.makeObject(middle_constraint - sf::Vector2f(200.f, 100.f), 2 + rand() % 18);
+            object.color = {static_cast<sf::Uint8> ( rand() % 255),static_cast<sf::Uint8> ( rand() % 255), static_cast<sf::Uint8> ( rand() % 255)};
+            
+        }
+
+        frames_in_second_vector.emplace_back(clock.getElapsedTime());
+
+        vector<sf::Time> temp_frames_vector;
+        for (sf::Time time : frames_in_second_vector) { 
+            if ((clock.getElapsedTime() - time).asSeconds() < 1) {
+                temp_frames_vector.emplace_back(time);
+            }
+        }
+        frames_in_second_vector = temp_frames_vector;
+        
+        cout << "Frames in last second: " << frames_in_second_vector.size() << endl;
+
+        solver.update();
+        window.clear(sf::Color::White);
+        renderer.render(solver);
+        window.display();
+    }
+
+    return 0;
+}
---
+++ sfml-test/Solver.hpp
@@ -0,0 +1,123 @@
+#pragma once
+#include <vector>
+#include <iostream>
+
+
+struct VerletObject {
+	sf::Vector2f position;
+	sf::Vector2f position_last;
+	sf::Vector2f acceleration;
+	float radius = 10.f;
+	sf::Color color = sf::Color::White;
+
+	VerletObject() = default;
+	VerletObject(sf::Vector2f position_, float radius_) {
+		position = position_;
+		position_last = position_;
+		radius = radius_;
+	}
+
+	void update(float dt) {
+		const sf::Vector2f displacement = position - position_last;
+		// Update position
+		position_last = position;
+		position = position + displacement + acceleration * (dt * dt);
+		// Reset acceleration
+		acceleration = {};
+	}
+
+	void accelerate(sf::Vector2f acceleration_) {
+		acceleration = acceleration_;
+	}
+	
+};
+
+class Solver {
+	public:
+		Solver() = default;
+
+		float dt = .005;
+
+		void update() {
+			applyGravity();
+			updateObjects(dt);
+			applyConstraint();
+			checkCollisions();
+		}
+
+		void applyGravity() {
+			for (auto& object : object_vector) {
+				object.accelerate(gravity_acceleration);
+			}
+		}
+
+		void setConstraint(sf::Vector2f position, float radius) {
+			constraint_position = position;
+			constraint_radius = radius;
+		}
+
+		sf::Vector3f getConstraint() const {
+			return {constraint_position.x, constraint_position.y, constraint_radius};
+		}
+
+		VerletObject& makeObject(sf::Vector2f position, float radius) {
+			VerletObject new_object{position, radius};
+			
+			object_vector.emplace_back(new_object);
+
+			return object_vector.at(object_vector.size() - 1);
+		}
+
+		const std::vector<VerletObject>& getObjects() const
+		{
+			return object_vector;
+		}
+
+		int getObjectCount() const {
+			return object_vector.size();
+		}
+		
+
+	private:
+		std::vector<VerletObject> object_vector;
+		sf::Vector2f constraint_position;
+		float constraint_radius;
+		sf::Vector2f gravity_acceleration { 0.f, 100.f };
+
+		void updateObjects(float dt) {
+			for (auto& object : object_vector) {
+				object.update(dt);
+			}
+		}
+
+		void applyConstraint() {
+			for (auto& object : object_vector) {
+				sf::Vector2f difference = constraint_position - object.position;
+				float distance = sqrt(difference.x * difference.x + difference.y * difference.y);
+				if (distance > constraint_radius - object.radius) {
+					sf::Vector2f unit_vector = difference / distance;
+					object.position = constraint_position - unit_vector * (constraint_radius - object.radius);
+				}
+			}
+		}
+
+		void checkCollisions() {
+			for (auto& object_1 : object_vector) {
+				for (auto& object_2 : object_vector) {
+					if (&object_1 == &object_2) {
+						continue;
+					}
+					sf::Vector2f difference = object_2.position - object_1.position;
+					float distance = sqrt(difference.x * difference.x + difference.y * difference.y);
+					sf::Vector2f unit_vector12 = difference / distance;
+
+					if (distance < object_1.radius + object_2.radius) {
+						float needed_move = (object_1.radius + object_2.radius) - distance;
+						
+						object_1.position -= unit_vector12 * needed_move * .5f;
+						object_2.position += unit_vector12 * needed_move * .5f;
+					}
+				}
+			}
+		}
+};
