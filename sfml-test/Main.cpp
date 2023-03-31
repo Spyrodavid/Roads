89aeeefaf179d61ad47d723c16c32c37b2808e19
David Saifer <spyrotheultimate@gmail.com>
2023-03-30 21:08:23 -0700

    Roads workin

--- sfml-test/Main.cpp
+++ sfml-test/Main.cpp
@@ -1,10 +1,14 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 #include <vector>
-#include "Solver.hpp"
-#include "Renderer.hpp"
+
+# define M_PIl          3.141592653589793238462643383279502884L /* pi */
 
 using namespace std;
+
+float rand_float01() {
+    return (rand() * 1.f) / RAND_MAX;
+}
 
 int main()
 {
@@ -12,19 +16,60 @@ int main()
     int window_width = 800;
     int window_height = 800;
 
+    int mouseX = 0;
+    int mouseY = 0;
+
 
     sf::RenderWindow window(sf::VideoMode(window_width, window_height), "Verlet Integration!!");
 
 
-    // setup vars for whole program
-    Solver solver;
-    Renderer renderer{ window };
-    sf::Vector2f middle_constraint = { static_cast<float> (window_width / 2), static_cast<float> (window_height / 2) };
-    float object_spawn_rate = .01;
-    int object_max = 2;
+    sf::Vector2f position(window_width / 2, window_height / 2);
+    float radius = 5.f;
+    srand(time(0));
 
-    // add constraint to solver
-    solver.setConstraint(middle_constraint, static_cast<float> (window_height / 2));
+
+    window.clear(sf::Color::Black);
+
+    int iterations = 1000;
+    int iterations_max = iterations;
+    
+
+    while (iterations > 0) {
+
+        float percent = (position.y / (window_height + radius));
+        float percent_pow = pow(percent, 4);
+        int color_percent = 50 + percent_pow * 205;
+
+
+       /* sf::CircleShape constraint_shape(radius);
+        constraint_shape.setOrigin(radius, radius);
+        constraint_shape.setFillColor(sf::Color(255 * (1.f * iterations / iterations_max), color_percent * .93, color_percent));
+        constraint_shape.setPosition(position.x, position.y);
+        constraint_shape.setPointCount(50);
+        window.draw(constraint_shape);*/
+
+
+        sf::Vertex line[2] =
+        {
+            sf::Vertex(sf::Vector2f(position.x, position.y))
+        };
+
+        
+
+        // Move
+        float angle = (M_PIl / 2) + (((rand() % 3) / 3.f) * M_PIl) * 2;
+        //cout << angle << endl;
+        position.y += sin(angle) * 20;
+        position.x += cos(angle) * 20;
+
+        line[1] = sf::Vertex(sf::Vector2f(position.x, position.y));
+
+        window.draw(line, 2, sf::Lines);
+
+        iterations--;
+    }
+
+    window.display();
 
 
 
@@ -46,30 +91,23 @@ int main()
         {
             switch (event.type) {
                 case sf::Event::Closed:
+
                     window.close();
                     break;
-                case sf::Event::KeyPressed:
-                    if (event.key.code == sf::Keyboard::Equal) {
-                        auto constraint = solver.getConstraint();
-                        solver.setConstraint({ constraint.x, constraint.y }, constraint.z - 3);
-                    }
-                    if (event.key.code == sf::Keyboard::Hyphen) {
-                        auto constraint = solver.getConstraint();
-                        solver.setConstraint({ constraint.x, constraint.y }, constraint.z + 3);
-                    }
 
+                case sf::Event::MouseMoved:
+                   
+                    mouseX = event.mouseMove.x;
+                    mouseY = event.mouseMove.y;
                     break;
+
                 default:
                     break;
             }
             
         }
 
-        while (clock.getElapsedTime().asSeconds() > solver.getObjectCount() * object_spawn_rate && solver.getObjectCount() < object_max) {
-            auto& object = solver.makeObject(middle_constraint - sf::Vector2f(200.f + (rand() % 10), 100.f), 20);
-            object.color = {static_cast<sf::Uint8> ( 100),static_cast<sf::Uint8> ( 100), static_cast<sf::Uint8> (( rand() % 2) * 255) };
-            
-        }
+  
 
         frames_in_second_vector.emplace_back(clock.getElapsedTime());
 
@@ -81,13 +119,16 @@ int main()
         }
         frames_in_second_vector = temp_frames_vector;
         
-        cout << "Frames in last second: " << frames_in_second_vector.size() << endl;
+        std::cout << "Frames in last second: " << frames_in_second_vector.size() << endl;
 
-        solver.update();
-        window.clear(sf::Color::White);
-        renderer.render(solver);
-        window.display();
+
+        
+
+        
+
+        //window.display();
     }
 
     return 0;
 }
+
